26d7bc42b123f48080af0815e156703e9e445ae7
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-08-20 18:25:04 +0300

    feat: implement link in each menu item element

--- src/components/view/main/side-menu/side-menu-item/side-menu-item.ts
+++ src/components/view/main/side-menu/side-menu-item/side-menu-item.ts
@@ -2,10 +2,13 @@ import Control from "control";
 
 export const createSideMenuItem = (sideMenuList: HTMLElement, title: string, iconName: string) => {
   const listItem = new Control(sideMenuList, "li", "side-menu-item");
-  new Control(listItem.node, "b");
-  new Control(listItem.node, "b");
-  const listItemIconBox = new Control(listItem.node, "span", "side-menu-item__icon");
+  const listItemLink = new Control(listItem.node, "a");
+  listItemLink.node.setAttribute("href", `/${title.toLowerCase()}`);
+
+  new Control(listItemLink.node, "b");
+  new Control(listItemLink.node, "b");
+  const listItemIconBox = new Control(listItemLink.node, "span", "side-menu-item__icon");
   const icon = new Control(listItemIconBox.node, "ion-icon");
   icon.node.setAttribute("name", iconName);
-  new Control(listItem.node, "span", "side-menu-item__title", title);
+  new Control(listItemLink.node, "span", "side-menu-item__title", title);
 }
