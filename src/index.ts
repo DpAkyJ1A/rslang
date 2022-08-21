8f0198e697a1569309ff42ac8ca83cbd77da465c
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-08-21 19:11:27 +0300

    refactor: implement ionicon by ts instead of html

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import 'Components/view/index.scss';
+import Control from './components/view/control';
+import { createLayout } from './components/view/app';
 
-import { createHeader } from './components/view/header/header';
-import { createSideMenu } from './components/view/side-menu/side-menu';
-import { createMain } from './components/view/main/main';
-import { createFooter } from './components/view/footer/footer';
+const body = document.body as HTMLBodyElement;
+let root = document.querySelector('#root') as HTMLElement | null;
+if (root === null) {
+    root = new Control(body, 'div').node;
+    root.id = 'root';
+}
 
-const root = document.querySelector('#root')! as HTMLElement;
+const ionIconModule = new Control(body, 'script');
+ionIconModule.node.setAttribute('type', 'module');
+ionIconModule.node.setAttribute('src', 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js');
 
-createHeader(root);
-createSideMenu(root);
-createMain(root);
-createFooter(root);
+const ionIconNoModule = new Control(body, 'script');
+ionIconNoModule.node.setAttribute('nomodule', '');
+ionIconNoModule.node.setAttribute('src', 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js');
+
+createLayout(root);
