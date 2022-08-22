38f8bca1a0ce54c379051f906267c77fb0b85758
romanovich_anton <anton.romanovich21@gmail.com>
2022-08-22 13:53:57 +0300

    refactor: move inline svg styles to card.scss

--- src/components/view/textbook/card/card.ts
+++ src/components/view/textbook/card/card.ts
@@ -47,9 +47,9 @@ export default class Card {
         const content = new Control(this.node, 'div', 'card__content');
         const header = new Control(content.node, 'div', 'card__header');
         header.node.innerHTML = `
-            <svg class="card__icon card__icon_folder" height="25px" width="25px"><path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"></path></svg>
+            <svg class="card__icon card__icon_folder"><path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"></path></svg>
             <h3 class="card__word">${this.word} - ${this.transcription}</h3>
-            <svg class="card__icon card__icon_play" onClick="() => this.playAudio()" height="25px" width="25px"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path></svg>
+            <svg class="card__icon card__icon_play" onClick="() => this.playAudio()"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path></svg>
             <audio src="${this.audio}"></audio>
             <audio src="${this.audioMeaning}"></audio>
             <audio src="${this.audioExample}"></audio>
