b3c6c412a8b953b2bae65deb676c10fef4231abf
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-08-22 03:02:56 +0300

    feat: implement beautiful footer

--- src/components/view/footer/footer.ts
+++ src/components/view/footer/footer.ts
@@ -1,6 +1,12 @@
 import Control from 'control';
+import { createFooterLogo } from './__logo/footer__logo';
+import { createFooterGit } from './__git/footer__git';
 
 export const createFooter = (root: HTMLElement) => {
     const footer = new Control(root, 'footer', 'footer');
-    new Control(footer.node, 'h1', 'footer__header', 'Footer');
+    createFooterLogo(footer.node);
+    createFooterGit(footer.node, 'DpAkyJ1A');
+    createFooterGit(footer.node, 'Katrinstom');
+    createFooterGit(footer.node, 'rmnvch');
+    new Control(footer.node, 'p', 'footer_year', '2022');
 };
