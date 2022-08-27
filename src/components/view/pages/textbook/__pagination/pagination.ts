833b4cee7dacb967d40849c16896538218084f66
romanovich_anton <anton.romanovich21@gmail.com>
2022-08-28 00:14:18 +0300

    fix: fix pagination logic

--- src/components/view/pages/textbook/__pagination/pagination.ts
+++ src/components/view/pages/textbook/__pagination/pagination.ts
@@ -4,13 +4,14 @@ import { route } from '../../../side-menu/side-menu-item/side-menu-item';
 const NUMBER_OF_PAGES = 30;
 const TOTAL_ELEMENTS_TO_DISPLAY = 9;
 
-export const createPgnEl = (page: number, group: number) => {
+export const createPgnEl = (pageInit: number, group: number) => {
     const wrapper = new Control(null, 'div', 'textbook__pagination pagination');
     const list = new Control(wrapper.node, 'ul', 'pagination__list');
+    const page = pageInit + 1;
 
     for (let i = 1; i <= TOTAL_ELEMENTS_TO_DISPLAY; i++) {
         const item = new Control(list.node, 'li', 'pagination__item');
-        const link = new Control(item.node, 'a');
+        const link = new Control(item.node, 'a', 'pagination__link');
         link.node.onclick = route;
 
         if (i === 1) {
@@ -90,9 +91,9 @@ export const createPgnEl = (page: number, group: number) => {
                 }
             }
         }
-        if (Number(link.node.innerHTML) === page) item.node.classList.add('pagination__item_active');
+        if (Number(link.node.innerHTML) === page) link.node.classList.add('pagination__link_active');
         if (!(link.node as HTMLAnchorElement).href) {
-            (link.node as HTMLAnchorElement).href = `/#textbook/?group=${group}&page=${link.node.innerHTML}`;
+            (link.node as HTMLAnchorElement).href = `/#textbook/?group=${group}&page=${+link.node.innerHTML - 1}`;
         }
     }
     return wrapper.node;
