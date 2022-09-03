bc942bf7e0185d0c77a119a596c202f3ea8a92c4
romanovich_anton <anton.romanovich21@gmail.com>
2022-09-03 18:50:03 +0300

    feat: add timer

---
+++ src/components/games/sprint/timer.ts/timer.ts
@@ -0,0 +1,64 @@
+import Control from '../../../view/control';
+
+const TIMER_IN_SPRINT = 10;
+
+// make sure we have only 1 timer instance - Singleton
+let instance: Timer | null;
+
+export default class Timer extends Control {
+    private wrapper: Control;
+    private display: Control;
+    private interval: ReturnType<typeof setInterval> | undefined;
+    private count: number;
+    constructor() {
+        super(null, 'div', 'timer');
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        if (!instance) instance = this;
+
+        this.wrapper = new Control(this.node, 'div', 'timer__wrapper timer__wrapper_animated');
+        this.display = new Control(this.wrapper.node, 'div', 'timer__display', `00:${TIMER_IN_SPRINT}`);
+        this.interval = undefined;
+        this.count = TIMER_IN_SPRINT;
+        return instance;
+    }
+
+    set displayValue(value: string) {
+        this.display.node.textContent = value;
+    }
+
+    get displayValue() {
+        return this.display.node.textContent as string;
+    }
+
+    start(container: HTMLElement) {
+        container.append(this.node);
+        // let count = TIMER_IN_SPRINT;
+        if (!this.interval) {
+            this.interval = setInterval(() => {
+                this.count--;
+                const value = this.count < 10 ? '0' + this.count : this.count;
+                this.displayValue = `00:${value}`;
+                if (this.count === 0) {
+                    clearInterval(this.interval);
+                    this.wrapper.node.classList.remove('timer__wrapper_animated');
+                    this.reset();
+                    this.alarmTimeout();
+                    // this.destroy();
+                }
+            }, 1000);
+        }
+    }
+
+    reset() {
+        this.interval = undefined;
+        this.display.node.innerHTML = `00:${TIMER_IN_SPRINT}`;
+        this.count = TIMER_IN_SPRINT;
+        this.wrapper.node.classList.add('timer__wrapper_animated');
+        instance = null;
+    }
+
+    alarmTimeout() {
+        const event = new Event('sprintTimeout');
+        document.dispatchEvent(event);
+    }
+}
