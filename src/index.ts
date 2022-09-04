8f2fe675e6d51a95042e32f13a0f9ce3539786d2
romanovich_anton <anton.romanovich21@gmail.com>
2022-09-04 21:52:57 +0300

    fix: fix isAuth value

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const state: IState = {
         group: 0,
     },
     user: {
-        isAuth: localStorage['tokenDataKata'] ? JSON.parse(localStorage['tokenDataKata']).userId : false,
+        isAuth: localStorage['tokenDataKata'] ? JSON.parse(localStorage['tokenDataKata']).isAuth : false,
         id: localStorage['tokenDataKata'] ? JSON.parse(localStorage['tokenDataKata']).userId : '',
         name: localStorage['tokenDataKata'] ? JSON.parse(localStorage['tokenDataKata']).name : '',
     },
@@ -37,7 +37,7 @@ const state: IState = {
 
 const app = new App(root, state);
 
-app.start({ isAuth: state.user.isAuth, name: state.user.name} );
+app.start({ isAuth: state.user.isAuth, name: state.user.name });
 
 interface ILocalStorageData {
     message: string;
@@ -49,9 +49,8 @@ interface ILocalStorageData {
 
 // const birds = new Control(body, 'script');
 // birds.node.setAttribute('type', 'module');
-// birds.node.setAttribute('src','three.r119.min.js'); 
+// birds.node.setAttribute('src','three.r119.min.js');
 
 // const birdsBody = new Control(body, 'script');
 // birdsBody.node.setAttribute('type', 'module');
-// birdsBody.node.setAttribute('src','vanta.birds.min.js'); 
-
+// birdsBody.node.setAttribute('src','vanta.birds.min.js');
