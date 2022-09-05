d3bf6dce15b240022dbc2e507bc2b94d4c2be458
Ekaterina Stom <ekaterinastom@gmail.com>
2022-09-06 01:22:55 +0300

    fix: title games

--- src/components/view/pages/games-page/games-page.ts
+++ src/components/view/pages/games-page/games-page.ts
@@ -21,10 +21,10 @@ export default class GamesPage extends Page {
         const gamesDiv = new Control(this.container, 'div', 'games__container');
         const divGame1 = new Control(gamesDiv.node, 'div', 'games__game1');
         const btnAudio = new Control(divGame1.node, 'button', 'games__audio-game');
-        new Control(divGame1.node, 'h3', 'games__title', 'sprint');
+        new Control(divGame1.node, 'h3', 'games__title', 'audiocall');
         const divGame2 = new Control(gamesDiv.node, 'div', 'games__game2');
         const btnSprint = new Control(divGame2.node, 'button', 'games__sprint');
-        new Control(divGame2.node, 'h3', 'games__title', 'audio game');
+        new Control(divGame2.node, 'h3', 'games__title', 'sprint');
         let sprint: SprintGame | null;
         btnSprint.node.onclick = () => {
             sprint = new SprintGame(SprintGameLaunchMode.textbook, this.container);
