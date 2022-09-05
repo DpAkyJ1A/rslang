5844376504202d8d012f6b00fd3cc4718cb0db57
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-09-06 02:05:52 +0300

    feat: add link on main page into logo

--- src/components/view/header/header.ts
+++ src/components/view/header/header.ts
@@ -4,7 +4,9 @@ import { createHeaderAuth } from './__auth/header__auth';
 export const createHeader = (root: HTMLElement, data: { isAuth: boolean; name: string }) => {
     const header = new Control(root, 'header', 'header');
     const headerWrapper = new Control(header.node, 'div', 'header__h1-wrapper');
-    new Control(headerWrapper.node, 'h1', 'header__h1', 'RSLang');
+    const headerLink = new Control(headerWrapper.node, 'a', 'header__h1-link')
+    headerLink.node.setAttribute('href', '#main');
+    new Control(headerLink.node, 'h1', 'header__h1', 'RSLang');
     createHeaderAuth(header.node, data);
     return header.node;
 };
