8c1bc97bd8a0f17b6a54ed649c9eb2ea696cfe97
romanovich_anton <anton.romanovich21@gmail.com>
2022-09-07 17:40:54 +0300

    feat: add links to game start

--- src/components/view/pages/games-page/games-page.ts
+++ src/components/view/pages/games-page/games-page.ts
@@ -3,6 +3,7 @@ import SprintGame from '../../../games/sprint/sprint';
 import { SprintGameLaunchMode } from '../../../games/sprint/types/index';
 import Page from '../page';
 import { createPageHeader } from '../common/pageHeader/pageHeader';
+import AudioGame from '../../../games/audio-game/audio-game';
 
 export default class GamesPage extends Page {
     constructor() {
@@ -19,7 +20,7 @@ export default class GamesPage extends Page {
         this.container.append(header);
         const gamesDiv = new Control(this.container, 'div', 'games__container');
         const divGame1 = new Control(gamesDiv.node, 'div', 'games__game1');
-        new Control(divGame1.node, 'button', 'games__audio-game');
+        const btnAudio = new Control(divGame1.node, 'button', 'games__audio-game');
         new Control(divGame1.node, 'h3', 'games__title', 'audiocall');
         const divGame2 = new Control(gamesDiv.node, 'div', 'games__game2');
         const btn = new Control(divGame2.node, 'button', 'games__sprint');
@@ -28,6 +29,10 @@ export default class GamesPage extends Page {
         btn.node.onclick = () => {
             sprint = new SprintGame(SprintGameLaunchMode.sideBar, this.container, user, undefined);
             sprint.start();
+        };
+        btnAudio.node.onclick = () => {
+            const audio = new AudioGame(SprintGameLaunchMode.sideBar, user);
+            audio.start(this.container);
         };
     }
 }
